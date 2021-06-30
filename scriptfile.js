f42c468860c03632a17eb8e1ac2d38d3c8d3caac
Mnenaji <86468493+Mnenaji@users.noreply.github.com>
2021-06-30 12:56:28 +0300

    Add files via upload

---
+++ scriptfile.js
@@ -0,0 +1,47 @@
+
+
+function jina(x)
+{
+	alert("You are on the photo of Ed, Edd and Eddy.");
+}
+
+function jinap(y)
+{
+	alert("You are on the photo of fairy odd parents.");
+}
+
+function jinao(z)
+{
+	alert("You are on the photo of Courage the Cowardly Dog.");
+}
+
+function jinaz(w)
+{
+	alert("You are on the photo of Tom and Jerry.");
+}
+
+function clicke(x)
+{
+	alert("You are now going Ed, Edd and Eddy.");
+}
+
+function clickf(y)
+{
+	alert("You are now going fairy odd parents.");
+}
+
+function clickc(z)
+{
+	alert("You are now going Courage the Cowardly Dog.");
+}
+
+function clickt(w)
+{
+	alert("You are now going Tom and Jerry.");
+}
+
+function home(p)
+{
+	alert("You are now going back to main home page.");
+}
+//onclick= "home(p);"  onmouseover= "jinaz();"
